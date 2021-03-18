18f650fe390528bcbaf66942967ee2ec2bf4ae29
8c6794b6 <8c6794b6@gmail.com>
2021-03-18 18:38:59 +0900

    Show path of stack

--- .github/actions/ps01/src/Main.purs
+++ .github/actions/ps01/src/Main.purs
@@ -2,12 +2,16 @@ module Main where
 
 import Prelude
 
-import Control.Monad.Except (runExceptT)
+import Control.Monad.Except (runExceptT, throwError)
 import Data.Either (Either(..))
 import Data.Maybe (Maybe(..))
 
 import Effect (Effect)
+import Effect.Class (liftEffect)
 import Effect.Exception (message)
+
+-- purescript-aff
+import Effect.Aff (Aff, launchAff_)
 
 -- purescript-node-process
 import Node.Process as Process
@@ -15,31 +19,43 @@ import Node.Process as Process
 -- purescript-js-date
 import Data.JSDate (now, toISOString)
 
--- purescript-github-actions-toolkig
+-- purescript-github-actions-toolkit
 import GitHub.Actions.Core as Core
+import GitHub.Actions.IO as IO
 
 main :: Effect Unit
-main = do
-  result <- runExceptT $
-     Core.getInput { name: "who-to-greet"
-                   , options: Just { required: true } }
+main = launchAff_ (getStack >>= work >>> liftEffect)
 
-  case result of
-    Right name -> greetAndSetOutput  name
+work :: String -> Effect Unit
+work stack = do
+  Core.info ("stack: " <> stack)
+
+  et_name <- runExceptT $
+    Core.getInput {name: "who-to-greet", options: Just {required: true}}
+
+  case et_name of
+    Right name -> greetAndSetOutput name
     Left err -> Core.setFailed (message err)
+
+getStack :: Aff String
+getStack = do
+  et_path <- runExceptT (IO.which {tool: "stack", check: Just true})
+  case et_path of
+    Right path -> pure path
+    Left err -> throwError err
 
 greetAndSetOutput :: String -> Effect Unit
 greetAndSetOutput name = do
   Core.info $ "Hello, " <> name <> "!"
 
   time <- now >>= toISOString
-  Core.setOutput { name: "time", value: time }
+  Core.setOutput {name: "time", value: time}
 
   let key = "GITHUB_ACTOR"
   mb_runner <- Process.lookupEnv key
   Core.info $ case mb_runner of
     Just runner -> key <> ": " <> runner
-    _ -> "NO GITHUB_RUNNER found in process"
+    _ -> "NO " <> key <> "found in process env"
 
   Core.info $ case Process.platform of
     Just pf -> "Platform: " <> show pf
