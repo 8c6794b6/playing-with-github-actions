141851f7a972c422497218ac6b1de70ba7546427
8c6794b6 <8c6794b6@gmail.com>
2023-10-26 15:31:52 +0900

    Add Hello.Foo.foo02

--- hs-hello/src/Hello/Foo.hs
+++ hs-hello/src/Hello/Foo.hs
@@ -15,3 +15,10 @@ foo01 a b = do
 
   when (0 <= a && 0 <= b) $
     putStrLn "0 <= a && 0 <= b"
+
+foo02 :: Int -> Int -> IO ()
+foo02 a b
+  | b == 0 = putStrLn "Cannot divide by zero."
+  | otherwise = case quotRem a b of
+      (q, 0) -> print q
+      _      -> print (fromIntegral a / fromIntegral b :: Double)
